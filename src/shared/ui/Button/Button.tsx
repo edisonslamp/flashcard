8b3f39b88ceda2432086bf7a0a95aaf67a3a967d
Eduard Miterev <nichegohair@gmail.com>
2023-10-05 14:13:01 +0200

    hotfix

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -2,13 +2,14 @@ import { ButtonHTMLAttributes, FC } from "react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     className?: string;
+    children: string;
 }
 
 export const Button: FC<ButtonProps> = (props) => {
-    const { className, type } = props;
+    const { type, children } = props;
     return (
-        <button type={type} className={className} {...props}>
-            custom button
+        <button type={type} {...props}>
+            {children}
         </button>
     );
 };
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,4 +1,3 @@
-import { classNames } from "src/shared/lib";
 import { Button } from "src/shared/ui";
 import cls from "./Navbar.module.scss";
 
@@ -7,14 +6,12 @@ export const Navbar = () => {
         <div className={cls.Navbar}>
             I'm Navbar
             <div>
-                <Button
-                    type="submit"
-                    className={classNames(cls.loginBtn, {}, [])}
-                />
-                <Button
-                    type="submit"
-                    className={classNames(cls.loginBtn, {}, [])}
-                />
+                <Button type="submit" className={cls.navbarBtn}>
+                    Login
+                </Button>
+                <Button type="submit" className={cls.navbarBtn}>
+                    Sign Up
+                </Button>
             </div>
         </div>
     );
